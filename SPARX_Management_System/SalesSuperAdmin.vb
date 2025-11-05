3b43fe48e19f220a8fb106fe4d7190eb1b96b2c0
Ann Dominique <estrada.ad@outlook.com>
2025-11-05 13:18:24 +0800

    ..

--- SPARX_Management_System/SalesSuperAdmin.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class SalesSuperAdmin
-    Private Sub DashboardBtn_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub SalesSuperAdmin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-End Class
